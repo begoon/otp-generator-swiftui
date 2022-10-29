3c58f9bbb3e90e98f6323259e05c1e44ea545111
Alexander Demin <ademin@gmail.com>
2022-10-29 13:24:23 +0100

    Auto-generated project

---
+++ OTP Maker/OTP Maker/OTP_MakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OTP_MakerApp.swift
+//  OTP Maker
+//
+//  Created by Alexander Demin on 29/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct OTP_MakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
