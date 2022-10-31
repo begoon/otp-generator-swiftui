7b17822a71d7e7013c6dcb84316223398f1e406b
Alexander Demin <ademin@gmail.com>
2022-10-31 17:57:52 +0000

    Add cli target and makefile

--- project/OTP/Accounts.swift
+++ project/OTP/Accounts.swift
@@ -8,22 +8,53 @@ struct Account: Codable {
 
 typealias Accounts = [Account]
 
-func parseAccounts(from text: String) -> Accounts {
-    guard let data = text.data(using: .utf8) else { return [] }
+enum ParseError: Error {
+    case invalidUTF8(text: String)
+    case invalidFileName(name: String)
+    case invalidFile(url: URL)
+}
+
+func parseAccounts(from text: String) throws -> Accounts {
+    guard let data = text.data(using: .utf8) else {
+        throw ParseError.invalidUTF8(text: text)
+    }
     return (try? JSONDecoder().decode(Accounts.self, from: data)) ?? []
 }
 
-let fileName = "Accounts.json"
+let defaultFileName = "Accounts.json"
+let defaultFileURL = FileManager.default.homeDirectoryForCurrentUser.appendingPathComponent(
+    defaultFileName
+)
 
-func loadAccounts() -> String {
-    let homeDirectory = FileManager.default.homeDirectoryForCurrentUser
-    let fileURL = homeDirectory.appendingPathComponent(fileName)
+func loadAccounts(from fileName: String = "") throws -> String {
+    var fileURL: URL = defaultFileURL
+    if !fileName.isEmpty {
+        guard let url = URL(string: "file://" + fileName) else {
+            throw ParseError.invalidFileName(name: fileName)
+        }
+        fileURL = url
+    }
     NSLog("loading accounts from %@", fileURL.absoluteString)
-    guard let content = try? String(contentsOf: fileURL) else { return "" }
+    guard let content = try? String(contentsOf: fileURL) else {
+        throw ParseError.invalidFile(url: fileURL)
+    }
     NSLog("loaded accounts: %@", content)
     return content
 }
 
-func accounts() -> Accounts {
-    return parseAccounts(from: loadAccounts())
+func accounts(from fileName: String = "") -> Accounts {
+    do {
+        let content = try loadAccounts(from: fileName)
+        return try parseAccounts(from: content)
+    } catch ParseError.invalidUTF8(let text) {
+        NSLog("invalid UTF8 in [%@]", text)
+        return []
+    } catch ParseError.invalidFileName(let name) {
+        NSLog("invalid filename [%@]", name)
+    } catch ParseError.invalidFile(let url) {
+        NSLog("unable to load from [%@]", url.description)
+    } catch {
+        print("Unexpected error: \(error).")
+    }
+    return []
 }
--- project/OTP/Cli.swift
+++ project/OTP/Cli.swift
@@ -1,10 +1,26 @@
+import Foundation
+
+let fileNameFlagPrefix = "--accounts="
+
 func cli() {
-    let accounts = accounts()
-    let name = CommandLine.arguments[1]
-    guard let account = (accounts.first { account in account.label == name }) else {
-        print("account", name, "not found")
+    let label = CommandLine.arguments[1]
+
+    var fileName = String()
+
+    if let flag = CommandLine.arguments.first(where: { value in
+        value.starts(with: fileNameFlagPrefix)
+    }) {
+        fileName = String(flag.suffix(flag.count - fileNameFlagPrefix.count))
+    }
+
+    let accounts = accounts(from: fileName)
+
+    guard let account = (accounts.first { account in account.label == label }) else {
+        NSLog("account [%@] not found", label)
         return
     }
+
     print(account)
+
     print(totp(secret: account.secret, format: account.format))
 }
